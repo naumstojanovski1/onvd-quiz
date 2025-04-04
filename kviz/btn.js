34c7e0c43591068d166c1d22cdbbc0c6bc28c00b
naumstojanovski1 <naum.stojanovski@students.finki.ukim.mk>
2025-04-04 19:52:15 +0200

    Delete kviz directory

--- kviz/btn.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("startBtn").addEventListener("click", function() {
-    // Hide the start screen and start the quiz
-    document.getElementById("startScreen").style.display = "none";
-    startQuiz();
-});
