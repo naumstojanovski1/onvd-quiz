f925a050b7889e0d7981ae064a4236165d60c727
naumstojanovski1 <naum.stojanovski@students.finki.ukim.mk>
2025-04-04 19:52:30 +0200

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,5 @@
+document.getElementById("startBtn").addEventListener("click", function() {
+    // Hide the start screen and start the quiz
+    document.getElementById("startScreen").style.display = "none";
+    startQuiz();
+});
